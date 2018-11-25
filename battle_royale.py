4a15355e8c1088297c342d870351b4ada5503e75
Martin <m.dupont@live.com.au>
2018-11-25 16:09:02 +0100

    battle_royale working

--- battle_royale.py
+++ battle_royale.py
@@ -6,36 +6,44 @@ Created on Sun Nov 25 11:02:00 2018
 """
 
 import math
-from itertools import permutations
+from itertools import combinations
 from multirounds import run_matches
 
-player_pool = [["DUMB", {}]]
-n_worlds_list = [1, 4, 8, 10, 15, 20, 50, 100]
+def print_scores(results):
+    temp = [(id_dict[i], score) for i, score in results.items()]
+    leaderboard = sorted(temp, key = lambda x: -x[1])
+    max_len = max([len(str(x[0])) for x in leaderboard])
+    print(max_len)
+    print("=============== Leaderboard ==================")
+    for p, score in leaderboard:
+        p_formatted = str(p).ljust(max_len)
+        print("{}: {}".format(p_formatted, score))
 
-for c in [0.25, 0.5, math.sqrt(2), 1.0]:
+player_pool = []
+n_worlds_list = [4, 8, 10, 15, 20, 50]
+
+for c in [0.5, math.sqrt(2), 2]:
     player_pool += [["MCTSPLUS", {"c": c}]]
     for n in n_worlds_list:
-        player_pool += [["PIMC", {"c":c, "n_worlds": n}]]
+        player_pool += [["PIMC", {"c": c, "n_worlds": n}]]
         
 n_players = len(player_pool)
 
 id_dict = {i: player for i, player in enumerate(player_pool)}
-n_matches = 1
+n_matches = 4
 results = {i: 0 for i in range(n_players)}
 
-for perm in permutations(range(n_players), 4):
-    # We choose permutations because player order matters.
+count = 0
+for perm in combinations(range(n_players), 4):
     tup = ([id_dict[i] for i in perm], n_matches)
-    print("========== new pairs ========")
-    print(perm)
-    print(tup)
     points_totals = run_matches(tup, verbose=True)
     for i, score in points_totals.items():
         results[perm[i]] += score
-    print(results)
         
-print("========== Final scores ===========")
-for i, score in results.items():
-    print("{} : {}".format(id_dict[i], score))
+    count += 1
+    if count % 100 == 0:
+        print_scores(results)
+        
+print_scores(results)
     
 
